d3b25f648f5623d564585d8d01e97d4975536379
Lucas Emanuel Silva <53888064+lucas3g@users.noreply.github.com>
2025-07-15 21:55:30 -0300

    feat: add home module and improve splash

--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:my_dreams/modules/auth/presentation/auth_page.dart';
 import 'package:my_dreams/modules/splash/presentation/splash_page.dart';
+import 'package:my_dreams/modules/home/presentation/home_page.dart';
 
 import '../domain/entities/named_routes.dart';
 import 'domain/entities/custom_transition.dart';
@@ -15,6 +16,7 @@ class CustomNavigator {
     final Map<String, WidgetBuilder> appRoutes = <String, WidgetBuilder>{
       NamedRoutes.splash.route: (BuildContext context) => const SplashPage(),
       NamedRoutes.auth.route: (BuildContext context) => const AuthPage(),
+      NamedRoutes.home.route: (BuildContext context) => const HomePage(),
     };
 
     final WidgetBuilder? builder = appRoutes[settings.name];
